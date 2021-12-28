fd20b4c8f35f8f660bcbe0115654ba48d90df3d0
MLDKYT <mldkyt@protonmail.com>
2021-12-28 21:13:46 +0100

    Add project files.

---
+++ OldTruck.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using MSCLoader;
+using OldTruck.Stuff;
+using UnityEngine;
+
+namespace OldTruck {
+    public class OldTruck : Mod {
+        public override string ID => "OldTruck";
+        public override string Name => "OldTruck";
+        public override string Author => "MLDKYT";
+        public override string Version => "b0.1";
+
+        public override bool UseAssetsFolder => false;
+
+
+        #region Variables
+
+        public GameObject Gifu;
+
+        public static AudioClip EngineStart1;
+        public static AudioClip EngineStart2;
+        public static GameObject TruckLogBed1;
+        public static GameObject TruckLogBed2;
+
+        #endregion
+        #region Settings
+
+        public static Settings RadioPosNumber;
+        public static Settings RemovePedalsToggle;
+        public static Settings RemoveGearStickToggle;
+        public static Settings RemoveDoorsToggle;
+        public static Settings ReplaceShitTankWithLogsToggle;
+        public static Settings OldStartingSoundToggle;
+
+        #endregion
+
+        public override void OnLoad() {
+            byte[] resBytes = Resources.oldtruck;
+            AssetBundle bundle = AssetBundle.CreateFromMemoryImmediate(resBytes);
+            if ((bool) OldStartingSoundToggle.Value) {
+                EngineStart1 = bundle.LoadAsset<AudioClip>("valmet_start");
+                EngineStart2 = bundle.LoadAsset<AudioClip>("valmet_starting");
+            }
+            if ((bool) ReplaceShitTankWithLogsToggle.Value) {
+                TruckLogBed1 = bundle.LoadAsset<GameObject>("truck_logbed");
+                TruckLogBed2 = bundle.LoadAsset<GameObject>("truck_logs");
+            }
+            bundle.Unload(false);
+
+            Gifu = GameObject.Find("GIFU(750/450psi)");
+            Gifu.AddComponent<Gifu>();
+        }
+
+        public override void ModSettings() {
+            RadioPosNumber = new Settings("radioPos", "Radio position", 0);
+            RemovePedalsToggle = new Settings("removePedals", "Remove pedals", false);
+            RemoveGearStickToggle = new Settings("removeGearStick", "Remove gear stick", false);
+            RemoveDoorsToggle = new Settings("removeDoors", "Remove doors", false);
+            ReplaceShitTankWithLogsToggle = new Settings("logs", "Replace shit tank with logs", false);
+            OldStartingSoundToggle = new Settings("oldStarting", "Old starting sound", false);
+
+            Settings.AddHeader(this,"Inside");
+            Settings.AddSlider(this, RadioPosNumber, 0, 2, new[] {
+                "Normal",
+                "Put it next to wipers knob",
+                "Remove radio"
+            });
+            Settings.AddCheckBox(this, RemovePedalsToggle);
+            Settings.AddCheckBox(this, RemoveGearStickToggle);
+            Settings.AddHeader(this, "Cabin");
+            Settings.AddCheckBox(this,RemoveDoorsToggle);
+            Settings.AddHeader(this, "Outside");
+            Settings.AddCheckBox(this, ReplaceShitTankWithLogsToggle);
+            Settings.AddHeader(this, "Sounds");
+            Settings.AddCheckBox(this, OldStartingSoundToggle);
+        }
+    }
+}
---
+++ Stuff/Gifu.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace OldTruck.Stuff {
+    public class Gifu : MonoBehaviour {
+        public GameObject GifuRadioPivot;
+        public GameObject GifuLod;
+        public GameObject GifuCabin;
+        public GameObject GifuMesh;
+        public GameObject GifuAudioEngine;
+
+        void Start() {
+            GifuRadioPivot = gameObject.transform.Find("RadioPivot").gameObject;
+            GifuLod = gameObject.transform.Find("LOD").gameObject;
+            GifuCabin = gameObject.transform.Find("Cabin").gameObject;
+            GifuMesh = gameObject.transform.Find("MESH").gameObject;
+
+            switch(OldTruck.RadioPosNumber.Value) {
+                case 1:
+                    GifuRadioPivot.transform.localPosition = new Vector3(-0.178f,1.729f,3.388f);
+                    GifuRadioPivot.transform.localRotation = Quaternion.Euler(new Vector3(-85.645f,-4.8f,184.795f));
+                    break;
+                case 2:
+                    GifuRadioPivot.SetActive(false);
+                    break;
+            }
+
+            if((bool)OldTruck.RemovePedalsToggle.Value) {
+                GifuLod.transform.Find("Dashboard/Pedals").gameObject.SetActive(false);
+            }
+
+            if ((bool) OldTruck.RemoveGearStickToggle.Value) {
+                GifuLod.transform.Find("Dashboard/GearLever").gameObject.SetActive(false);
+            }
+
+            if ((bool) OldTruck.ReplaceShitTankWithLogsToggle.Value) {
+                GifuMesh.transform.Find("shit_tank 1").gameObject.SetActive(false);
+                GifuLod.transform.Find("ShitTank").gameObject.SetActive(false);
+                GifuLod.transform.Find("work_lights").gameObject.SetActive(false);
+                transform.Find("ShitTank").gameObject.SetActive(false);
+                if ((bool) OldTruck.ReplaceShitTankWithLogsToggle.Value) {
+                    GameObject logsHolder = Instantiate(OldTruck.TruckLogBed1);
+                    logsHolder.transform.parent = GifuMesh.transform;
+                    logsHolder.transform.position = Vector3.zero;
+                    logsHolder.transform.rotation = Quaternion.identity;
+                    logsHolder.transform.localPosition = new Vector3(-0.08f,1.52f,0.8f);
+                    logsHolder.transform.localRotation = new Quaternion(-5.960465E-08f,2.107342E-08f,-2.107343E-08f,1f);
+                    logsHolder.transform.localScale = new Vector3(0.95f,0.98f,0.74f);
+                    GameObject logs = Instantiate(OldTruck.TruckLogBed2);
+                    logs.transform.parent = GifuMesh.transform;
+                    logs.transform.position = Vector3.zero;
+                    logs.transform.rotation = Quaternion.identity;
+                    logs.transform.localPosition = new Vector3(-0.08f,1.52f,0.8f);
+                    logs.transform.localRotation = new Quaternion(-5.960465E-08f,2.107342E-08f,-2.107343E-08f,1f);
+                    logs.transform.localScale = new Vector3(0.95f,0.98f,0.74f);
+                }
+            }
+
+            if ((bool) OldTruck.OldStartingSoundToggle.Value) {
+                GifuAudioEngine = UnityEngine.Resources.FindObjectsOfTypeAll<GameObject>().First(x => x.name == "AudioEngineGifu");
+                foreach (PlayMakerFSM playMakerFsm in GifuAudioEngine.GetComponents<PlayMakerFSM>()) {
+                    Destroy(playMakerFsm);
+                }
+            }
+        }
+    }
+}
