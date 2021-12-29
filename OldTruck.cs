22ea17338d1ac391ca1b731d6b396e716959fdea
MLDKYT <mldkyt@protonmail.com>
2021-12-29 12:49:21 +0100

    Release 1.0

--- OldTruck.cs
+++ OldTruck.cs
@@ -8,7 +8,7 @@ namespace OldTruck {
         public override string ID => "OldTruck";
         public override string Name => "OldTruck";
         public override string Author => "MLDKYT";
-        public override string Version => "b0.1";
+        public override string Version => "1.0";
 
         public override bool UseAssetsFolder => false;
 
@@ -19,60 +19,127 @@ namespace OldTruck {
 
         public static AudioClip EngineStart1;
         public static AudioClip EngineStart2;
+        public static AudioClip EngineIdle;
+        public static AudioClip EngineAccel;
+        public static AudioClip NewEngineIdle;
+        public static AudioClip NewEngineAccel;
         public static GameObject TruckLogBed1;
         public static GameObject TruckLogBed2;
+        public static Material Paint4;
+        public static Material TruckCabin;
 
         #endregion
         #region Settings
 
         public static Settings RadioPosNumber;
+        public static Settings EngineNumber;
         public static Settings RemovePedalsToggle;
         public static Settings RemoveGearStickToggle;
         public static Settings RemoveDoorsToggle;
         public static Settings ReplaceShitTankWithLogsToggle;
         public static Settings OldStartingSoundToggle;
+        public static Settings PaintInteriorBlackToggle;
+        public static Settings PaintExteriorRedToggle;
+        public static Settings HideSPZToggle;
+        public static Settings HideMudFlaps;
 
         #endregion
 
         public override void OnLoad() {
             byte[] resBytes = Resources.oldtruck;
             AssetBundle bundle = AssetBundle.CreateFromMemoryImmediate(resBytes);
-            if ((bool) OldStartingSoundToggle.Value) {
-                EngineStart1 = bundle.LoadAsset<AudioClip>("valmet_start");
-                EngineStart2 = bundle.LoadAsset<AudioClip>("valmet_starting");
+
+            if((bool)OldStartingSoundToggle.Value) {
+                EngineStart1 = bundle.LoadAsset<AudioClip>("valmetstart");
+                EngineStart2 = bundle.LoadAsset<AudioClip>("valmetstarting");
             }
+
             if ((bool) ReplaceShitTankWithLogsToggle.Value) {
-                TruckLogBed1 = bundle.LoadAsset<GameObject>("truck_logbed");
-                TruckLogBed2 = bundle.LoadAsset<GameObject>("truck_logs");
+                TruckLogBed1 = bundle.LoadAsset<GameObject>("trucklogbed.prefab");
+                TruckLogBed2 = bundle.LoadAsset<GameObject>("trucklogs.prefab");
+            }
+
+            if (EngineNumber.Value.ToString() != "0") {
+                switch (EngineNumber.Value.ToString()) {
+                    case "1":
+                        NewEngineIdle = bundle.LoadAsset<AudioClip>("newcumminsidle");
+                        NewEngineAccel = bundle.LoadAsset<AudioClip>("newcumminsaccel");
+                        break;
+                    case "2":
+                        EngineIdle = bundle.LoadAsset<AudioClip>("cumminsidle");
+                        EngineAccel = bundle.LoadAsset<AudioClip>("cumminsaccel");
+                        break;
+                }
+            }
+
+            if ((bool) PaintInteriorBlackToggle.Value) {
+                Paint4 = bundle.LoadAsset<Material>("paint4");
+            }
+
+            if ((bool) PaintExteriorRedToggle.Value) {
+                TruckCabin = bundle.LoadAsset<Material>("truckcabin");
             }
             bundle.Unload(false);
 
             Gifu = GameObject.Find("GIFU(750/450psi)");
             Gifu.AddComponent<Gifu>();
+
+            // Start sounds
+            if ((bool) OldStartingSoundToggle.Value) {
+                GameObject truckSoundsGameObject = GameObject.Find("MasterAudio/Truck");
+                truckSoundsGameObject.transform.Find("start1").GetComponent<AudioSource>().clip = EngineStart1;
+                truckSoundsGameObject.transform.Find("start2").GetComponent<AudioSource>().clip = EngineStart2;
+                truckSoundsGameObject.transform.Find("start3").GetComponent<AudioSource>().clip = EngineStart2;
+                truckSoundsGameObject.transform.Find("start4").GetComponent<AudioSource>().clip = EngineStart2;
+            }
         }
 
         public override void ModSettings() {
-            RadioPosNumber = new Settings("radioPos", "Radio position", 0);
-            RemovePedalsToggle = new Settings("removePedals", "Remove pedals", false);
-            RemoveGearStickToggle = new Settings("removeGearStick", "Remove gear stick", false);
-            RemoveDoorsToggle = new Settings("removeDoors", "Remove doors", false);
-            ReplaceShitTankWithLogsToggle = new Settings("logs", "Replace shit tank with logs", false);
-            OldStartingSoundToggle = new Settings("oldStarting", "Old starting sound", false);
+            RadioPosNumber = new Settings("radioPos","Radio position",0);
+            EngineNumber = new Settings("engineSel", "Engine type", 0);
+            RemovePedalsToggle = new Settings("removePedals","Remove pedals",false);
+            RemoveGearStickToggle = new Settings("removeGearStick","Remove gear stick",false);
+            RemoveDoorsToggle = new Settings("removeDoors","Remove doors",false);
+            ReplaceShitTankWithLogsToggle = new Settings("logs","Replace shit tank with logs",false);
+            OldStartingSoundToggle = new Settings("oldStarting","Old starting sound",false);
+            PaintInteriorBlackToggle = new Settings("blackInt", "Black interior", false);
+            PaintExteriorRedToggle = new Settings("redExt", "Red exterior", false);
+            HideSPZToggle = new Settings("spzHide", "Hide license plate", false);
+            HideMudFlaps = new Settings("hideFlaps", "Hide mud flaps", false);
 
             Settings.AddHeader(this,"Inside");
-            Settings.AddSlider(this, RadioPosNumber, 0, 2, new[] {
+            Settings.AddSlider(this,RadioPosNumber,0,2,new[] {
                 "Normal",
                 "Put it next to wipers knob",
                 "Remove radio"
             });
-            Settings.AddCheckBox(this, RemovePedalsToggle);
-            Settings.AddCheckBox(this, RemoveGearStickToggle);
-            Settings.AddHeader(this, "Cabin");
+            Settings.AddCheckBox(this,RemovePedalsToggle);
+            Settings.AddCheckBox(this,RemoveGearStickToggle);
+            Settings.AddHeader(this,"Cabin");
             Settings.AddCheckBox(this,RemoveDoorsToggle);
-            Settings.AddHeader(this, "Outside");
-            Settings.AddCheckBox(this, ReplaceShitTankWithLogsToggle);
-            Settings.AddHeader(this, "Sounds");
-            Settings.AddCheckBox(this, OldStartingSoundToggle);
+            Settings.AddCheckBox(this, PaintInteriorBlackToggle);
+            Settings.AddHeader(this,"Outside");
+            Settings.AddCheckBox(this,ReplaceShitTankWithLogsToggle);
+            Settings.AddCheckBox(this, PaintExteriorRedToggle);
+            Settings.AddCheckBox(this, HideSPZToggle);
+            Settings.AddCheckBox(this, HideMudFlaps);
+            Settings.AddHeader(this,"Sounds");
+            Settings.AddSlider(this, EngineNumber, 0, 2, new[] {
+                "Keep these sounds",
+                "Use pre-2018 sounds",
+                "Use build 172 sounds"
+            });
+            Settings.AddCheckBox(this,OldStartingSoundToggle);
+        }
+
+        public static void PrintF(string s) {
+            ModConsole.Print(string.Format("[OldTruck] {0}",s));
+        }
+
+        public static void PrintE(string s, Exception e) {
+            ModConsole.Warning(string.Format("[OldTruck:Error] {0}: {1}", s, e.Message));
         }
     }
+
+
 }
--- Stuff/Gifu.cs
+++ Stuff/Gifu.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-using System.Text;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace OldTruck.Stuff {
@@ -10,58 +10,229 @@ namespace OldTruck.Stuff {
         public GameObject GifuMesh;
         public GameObject GifuAudioEngine;
 
-        void Start() {
+        public SoundController Controller;
+        public Drivetrain Drivetrain;
+
+        public AudioSource LowAudioSource;
+        public AudioSource HighAudioSource;
+
+        private void Start() {
             GifuRadioPivot = gameObject.transform.Find("RadioPivot").gameObject;
             GifuLod = gameObject.transform.Find("LOD").gameObject;
             GifuCabin = gameObject.transform.Find("Cabin").gameObject;
             GifuMesh = gameObject.transform.Find("MESH").gameObject;
+            GifuAudioEngine = UnityEngine.Resources.FindObjectsOfTypeAll<GameObject>()
+                .First(x => x.name == "AudioEngineGifu");
+            Controller = gameObject.GetComponent<SoundController>();
+            Drivetrain = gameObject.GetComponent<Drivetrain>();
 
-            switch(OldTruck.RadioPosNumber.Value) {
-                case 1:
-                    GifuRadioPivot.transform.localPosition = new Vector3(-0.178f,1.729f,3.388f);
-                    GifuRadioPivot.transform.localRotation = Quaternion.Euler(new Vector3(-85.645f,-4.8f,184.795f));
-                    break;
-                case 2:
-                    GifuRadioPivot.SetActive(false);
-                    break;
+            try {
+                switch(OldTruck.RadioPosNumber.Value) {
+                    case 1:
+                        GifuRadioPivot.transform.localPosition = new Vector3(-0.178f,1.729f,3.388f);
+                        GifuRadioPivot.transform.localRotation =
+                            Quaternion.Euler(new Vector3(-85.645f,-4.8f,184.795f));
+                        break;
+                    case 2:
+                        GifuRadioPivot.SetActive(false);
+                        break;
+                }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Radio pos",e);
             }
 
             if((bool)OldTruck.RemovePedalsToggle.Value) {
-                GifuLod.transform.Find("Dashboard/Pedals").gameObject.SetActive(false);
+                try {
+                    GifuLod.transform.Find("Dashboard/Pedals").gameObject.SetActive(false);
+                }
+                catch(Exception e) {
+                    OldTruck.PrintE("Pedals",e);
+                }
             }
 
-            if ((bool) OldTruck.RemoveGearStickToggle.Value) {
-                GifuLod.transform.Find("Dashboard/GearLever").gameObject.SetActive(false);
+            if((bool)OldTruck.RemoveGearStickToggle.Value) {
+                try {
+                    GifuLod.transform.Find("Dashboard/GearLever").gameObject.SetActive(false);
+                }
+                catch(Exception e) {
+                    OldTruck.PrintE("Gear lever",e);
+                }
             }
 
-            if ((bool) OldTruck.ReplaceShitTankWithLogsToggle.Value) {
-                GifuMesh.transform.Find("shit_tank 1").gameObject.SetActive(false);
-                GifuLod.transform.Find("ShitTank").gameObject.SetActive(false);
-                GifuLod.transform.Find("work_lights").gameObject.SetActive(false);
-                transform.Find("ShitTank").gameObject.SetActive(false);
-                if ((bool) OldTruck.ReplaceShitTankWithLogsToggle.Value) {
+            if((bool)OldTruck.ReplaceShitTankWithLogsToggle.Value) {
+                try {
+                    GifuMesh.transform.Find("shit_tank 1").gameObject.SetActive(false);
+                    GifuLod.transform.Find("ShitTank").gameObject.SetActive(false);
+                    GifuLod.transform.Find("work_lights").gameObject.SetActive(false);
+                    transform.Find("ShitTank").gameObject.SetActive(false);
+                }
+                catch(Exception e) {
+                    OldTruck.PrintE("Disabling ShitTank error",e);
+                }
+
+                try {
                     GameObject logsHolder = Instantiate(OldTruck.TruckLogBed1);
                     logsHolder.transform.parent = GifuMesh.transform;
                     logsHolder.transform.position = Vector3.zero;
                     logsHolder.transform.rotation = Quaternion.identity;
                     logsHolder.transform.localPosition = new Vector3(-0.08f,1.52f,0.8f);
-                    logsHolder.transform.localRotation = new Quaternion(-5.960465E-08f,2.107342E-08f,-2.107343E-08f,1f);
+                    logsHolder.transform.localRotation =
+                        new Quaternion(-5.960465E-08f,2.107342E-08f,-2.107343E-08f,1f);
                     logsHolder.transform.localScale = new Vector3(0.95f,0.98f,0.74f);
                     GameObject logs = Instantiate(OldTruck.TruckLogBed2);
                     logs.transform.parent = GifuMesh.transform;
                     logs.transform.position = Vector3.zero;
                     logs.transform.rotation = Quaternion.identity;
-                    logs.transform.localPosition = new Vector3(-0.08f,1.52f,0.8f);
+                    logs.transform.localPosition = new Vector3(-0.08f,1.52f,1.37F);
                     logs.transform.localRotation = new Quaternion(-5.960465E-08f,2.107342E-08f,-2.107343E-08f,1f);
                     logs.transform.localScale = new Vector3(0.95f,0.98f,0.74f);
                 }
+                catch(Exception e) {
+                    OldTruck.PrintE("Spawning Logs",e);
+                }
             }
 
-            if ((bool) OldTruck.OldStartingSoundToggle.Value) {
-                GifuAudioEngine = UnityEngine.Resources.FindObjectsOfTypeAll<GameObject>().First(x => x.name == "AudioEngineGifu");
-                foreach (PlayMakerFSM playMakerFsm in GifuAudioEngine.GetComponents<PlayMakerFSM>()) {
-                    Destroy(playMakerFsm);
+            try {
+                if(OldTruck.EngineNumber.Value.ToString() != "0") {
+                    foreach(PlayMakerFSM fsm in GifuAudioEngine.GetComponents<PlayMakerFSM>()) {
+                        if(fsm.name != "Throttle") {
+                            fsm.enabled = false;
+                        }
+                    }
                 }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Destroying engine",e);
+            }
+
+            try {
+                if(OldTruck.EngineNumber.Value.ToString() != "0") {
+                    GameObject go1 = new GameObject("LowSound");
+                    LowAudioSource = go1.AddComponent<AudioSource>();
+                    LowAudioSource.spatialBlend = 1f;
+                    LowAudioSource.maxDistance = 25f;
+                    GameObject go2 = new GameObject("HighSound");
+                    HighAudioSource = go2.AddComponent<AudioSource>();
+                    HighAudioSource.spatialBlend = 1f;
+                    HighAudioSource.maxDistance = 35f;
+                    switch(OldTruck.EngineNumber.Value.ToString()) {
+                        case "1":
+                            LowAudioSource.clip = OldTruck.NewEngineIdle;
+                            HighAudioSource.clip = OldTruck.NewEngineAccel;
+                            break;
+                        case "2":
+                            LowAudioSource.clip = OldTruck.EngineIdle;
+                            HighAudioSource.clip = OldTruck.EngineAccel;
+                            break;
+                        default:
+                            throw new InvalidOperationException("What the fuck?");
+                    }
+
+                    LowAudioSource.loop = true;
+                    HighAudioSource.loop = true;
+                    LowAudioSource.Play();
+                    HighAudioSource.Play();
+                }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Engine spawn",e);
+            }
+
+            try {
+                if((bool)OldTruck.PaintExteriorRedToggle.Value) {
+                    GifuMesh.transform.Find("truck_frame/truck_cabin 1").GetComponent<Renderer>().sharedMaterial =
+                        OldTruck.TruckCabin;
+                    Transform doorL = transform.Find("Cabin/DriverDoors/doorl/door");
+                    Transform doorR = transform.Find("Cabin/DriverDoors/doorr/door");
+                    doorL.GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorR.GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorL.Find("truck_cabin_door_panel").GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorR.Find("truck_cabin_door_panel 1").GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorL.Find("truck_cabin_door_parts").GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorR.Find("truck_cabin_door_parts 1").GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    doorL.Find("opener/mesh").GetComponent<Renderer>().sharedMaterial = OldTruck.TruckCabin;
+                    GifuMesh.transform.Find("truck_frame").GetComponent<Renderer>().sharedMaterial =
+                        OldTruck.TruckCabin;
+                }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Paint ext red",e);
+            }
+
+            try {
+                if((bool)OldTruck.PaintInteriorBlackToggle.Value) {
+                    Transform truckInterior = GifuLod.transform.Find("truck_interior");
+                    truckInterior.GetComponent<Renderer>().sharedMaterial = OldTruck.Paint4;
+                    truckInterior.Find("truck_interior_parts").GetComponent<Renderer>().sharedMaterial =
+                        OldTruck.Paint4;
+                }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Paint int black",e);
+            }
+
+            try {
+                GifuLod.transform.Find("truck_accessories").gameObject.SetActive(false);
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Remove mudflaps",e);
+            }
+
+            try {
+                GifuLod.transform.GetChild(3).gameObject.SetActive(false);
+                GifuLod.transform.GetChild(4).gameObject.SetActive(false);
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Remove SPZ",e);
+            }
+        }
+
+        private void Update() {
+            try {
+                if(OldTruck.EngineNumber.Value.ToString() != "0") {
+                    float num1 = Drivetrain.rpm / Drivetrain.maxRPM;
+                    float engineThrottleVolume;
+                    float engineNoThrottleVolume;
+                    float engineThrottlePitchFactor;
+                    float engineNoThrottlePitchFactor;
+
+                    if(Drivetrain.rpm < 300) {
+                        LowAudioSource.volume = 0f;
+                        HighAudioSource.volume = 0f;
+                    }
+                    else {
+                        LowAudioSource.transform.position = transform.position;
+                        HighAudioSource.transform.position = transform.position;
+                        switch(OldTruck.EngineNumber.Value.ToString()) {
+                            case "1":
+                                engineThrottleVolume = 1f;
+                                engineNoThrottleVolume = 0.5F;
+                                engineThrottlePitchFactor = .65f;
+                                engineNoThrottlePitchFactor = 1.5f;
+                                LowAudioSource.volume = Math.Abs(1f - Drivetrain.throttle) * engineNoThrottleVolume * num1;
+                                LowAudioSource.pitch = 0.5f + engineNoThrottlePitchFactor * num1;
+                                HighAudioSource.volume = Drivetrain.throttle * engineThrottleVolume + num1 * 0.200000002980232f;
+                                HighAudioSource.pitch = 0.5f + engineThrottlePitchFactor * num1;
+                                break;
+                            case "2":
+                                engineThrottleVolume = 1f;
+                                engineNoThrottleVolume = .5f;
+                                engineThrottlePitchFactor = .6f;
+                                engineNoThrottlePitchFactor = 1f;
+                                LowAudioSource.volume = Math.Abs(1f - Drivetrain.throttle) * engineNoThrottleVolume * num1;
+                                LowAudioSource.pitch = 0.5f + engineNoThrottlePitchFactor * num1;
+                                HighAudioSource.volume = Drivetrain.throttle * engineThrottleVolume + num1 * 0.200000002980232f;
+                                HighAudioSource.pitch = 0.5f + engineThrottlePitchFactor * num1;
+                                break;
+                            default:
+                                throw new InvalidOperationException("This is not a valid engine state...");
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+                OldTruck.PrintE("Sound change",e);
             }
         }
     }
